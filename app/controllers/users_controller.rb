b46f2b28c954bf2c80d3083d615285645f3ec8c6
nebulatide <nebulatide@gmail.com>
2014-07-01 13:40:56 -0400

    Add user microposts without test code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -55,13 +56,15 @@ class UsersController < ApplicationController
     end
 
     # Before filters
-
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
+    
+    # This has been moved to app/helper/sessions_helper.rb 
+    #def signed_in_user
+    #  unless signed_in?
+    #    store_location
+    #    redirect_to signin_url, notice: "Please sign in."
+    #  end
+    #end
+    
 
     def correct_user
       @user = User.find(params[:id])
